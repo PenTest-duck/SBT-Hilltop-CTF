5b377a0314591d53d12f2dbb914ce8ee031c68c9
PenTest-duck <51822841+PenTest-duck@users.noreply.github.com>
2020-01-19 17:35:48 +1100

    Update password_mangler.sh

--- Password Cracking/Shady - Writeup/password_mangler.sh
+++ Password Cracking/Shady - Writeup/password_mangler.sh
@@ -2,5 +2,5 @@
 
 cat $1 | while read -r passwd; do
 	length=$(($(echo $passwd | wc -m)+2))  # Set +n to number of chars to add - 1
-	crunch $length $length -t $passwd%%% >> wordlist2.txt  # Change the number of %s to n + 1
+	crunch $length $length -t $passwd%%% >> wordlist2.txt  # Set the number of %s to n + 1
 done
